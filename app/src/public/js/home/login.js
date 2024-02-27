526d7d03e89b2ebd1e183cfc4e8203b8f3784b9a
RollMocha <hwangjh5459@gmail.com>
2024-02-27 10:22:18 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -8,17 +8,27 @@ loginBtn.addEventListener("click", login);
 
 function login(){
     const req = {
-        id:id.value,
+        id: id.value,
         password: password.value,
     };
-    console.log(req);
-    console.log(JSON.stringify(req));
 
-    fetch("/login",{
+    fetch("/login", {
         method: "POST",
         headers: {
             "Content-Type":"application/json"
-        }
+        },
         body: JSON.stringify(req),
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        if(res.success){
+            location.href = "/";
+        }
+        else{
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error(new Error("로그인 중 에러 발생"));
     });
 }
